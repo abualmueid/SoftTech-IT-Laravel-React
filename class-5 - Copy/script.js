3bbe72f90eade770f0d086a4e99e4ec3a8a5b9f2
abualmueid <61595430+abualmueid@users.noreply.github.com>
2023-01-03 21:59:35 +0600

    added files

---
+++ class-5 - Copy/script.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function()
+{
+    jQuery('.clickhere').click(function()
+    {
+        $.ajax({
+            url: "process.php",
+            type: "post",
+            data: 
+            {
+                name: 'Mueid',
+                address: "Narayanganj"
+            },
+
+            success: function (response) {
+                jQuery('.information').html(response);
+            }
+        });
+            
+        return false;
+    });
+});
